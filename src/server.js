f02c8ff23f3017ca300e8c44493e83840627622a
Diego <dcorreanavarrete@gmail.com>
2022-06-22 13:34:42 -0500

    spliting server.js file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
+const { sequelize } = require("../models");
 require("dotenv/config");
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
+  await sequelize.sync();
   console.log(`Listening on http://localhost:${process.env.PORT}`);
 });
