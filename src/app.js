b4d414cd9a0ef8624c14d4a10a6e6e0b5467bcc8
Diego <dcorreanavarrete@gmail.com>
2022-06-22 21:13:13 -0500

    importing routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { authRouter, productRouter } = require("./routes/index");
 
 app.use(cors());
 app.use(express.json());
+app.use(authRouter, productRouter);
 
 module.exports = app;
