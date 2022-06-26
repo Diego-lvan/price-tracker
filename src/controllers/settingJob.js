3a78c1c21737a75a1f5cc53e85149eeccd1449d1
Diego <dcorreanavarrete@gmail.com>
2022-06-26 13:54:39 -0500

    function that updates the prices

--- src/controllers/settingJob.js
+++ src/controllers/settingJob.js
@@ -6,17 +6,19 @@ const { generateUrlFromId } = require("../utils/url");
 const setJob = async () => {
   const cronExpression = "*/1 * * * * *";
   const job = schedule.scheduleJob("test", cronExpression, async () => {
+    job.cancel();
     const updatedProducts = await fetchUpdatedProducts();
+    await updateProductPrices(updatedProducts);
   });
 };
 
 const fetchUpdatedProducts = async () => {
-  const products = await Product.findAll({ include: HistoryPrices });
+  const products = await Product.findAll({ include: HistoryPrices, order: [["createdAt", "DESC"]] });
   const ans = [];
   await Promise.allSettled(
     await products.map(async (product) => {
       const last = product.dataValues.HistoryPrices.length - 1;
-      const lastPrice = product.dataValues.HistoryPrices[last].dataValues.price;
+      const lastPrice = product.dataValues.HistoryPrices[0].dataValues.price;
       const url = generateUrlFromId(product.productID);
       const updatedProduct = await getUpdatedProduct(url, product.productID);
       if (updatedProduct.price != lastPrice) {
@@ -27,6 +29,10 @@ const fetchUpdatedProducts = async () => {
     })
   );
   return ans;
+};
+
+const updateProductPrices = async (products) => {
+  await Promise.allSettled(products.map(async ({ price, productID }) => await HistoryPrices.create({ price, productID })));
 };
 
 setJob();
