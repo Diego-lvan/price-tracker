8c29489968b6b53f21685b3fbba8e0991aff4ac4
Diego <dcorreanavarrete@gmail.com>
2022-06-29 17:09:43 -0500

    add try catch

--- src/controllers/scraper.js
+++ src/controllers/scraper.js
@@ -1,21 +1,25 @@
 const puppeteer = require("puppeteer");
 
 const getProductData = async (url, id) => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(url);
-  const data = await page.evaluate(() => {
-    const price = parseFloat(document.querySelector(".a-offscreen")?.textContent.replace("$", ""));
-    const title = document.querySelector("#productTitle")?.textContent.trim();
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url);
+    const data = await page.evaluate(() => {
+      const price = parseFloat(document.querySelector(".a-offscreen")?.textContent.replace("$", ""));
+      const title = document.querySelector("#productTitle")?.textContent.trim();
 
-    const product = { price, title };
+      const product = { price, title };
+      return product;
+    });
+
+    browser.close();
+
+    const product = { ...data, productID: id };
     return product;
-  });
-
-  browser.close();
-
-  const product = { ...data, productID: id };
-  return product;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = getProductData;
