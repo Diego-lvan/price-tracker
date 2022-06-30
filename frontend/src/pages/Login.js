7bd4ce23c4cd7fa2b2b414e61f28b91fd9d2fb1f
Diego <dcorreanavarrete@gmail.com>
2022-06-30 17:56:44 -0500

    updatin paths in Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Axios from "axios";
-import AuthLogin from "./AuthForm";
-import { URL } from "../../config";
+import AuthLogin from "../components/Auth/AuthForm";
+import { URL } from "../config";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
