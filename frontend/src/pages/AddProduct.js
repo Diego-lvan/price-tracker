d9b33407fcb5285a921bc24e0526e4500933d630
Diego <dcorreanavarrete@gmail.com>
2022-07-02 10:50:01 -0500

    add success and error messages

--- frontend/src/pages/AddProduct.js
+++ frontend/src/pages/AddProduct.js
@@ -5,21 +5,39 @@ import Form from "react-bootstrap/Form";
 import { URL } from "../config";
 const AddProduct = () => {
   const [productURL, setProductURL] = useState("");
-
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
   const addProduct = async (e) => {
     e.preventDefault();
-    const token = window.localStorage.getItem("token");
-    console.log(token);
-    const res = await axios.post(`${URL}/api/product`, { url: productURL }, { headers: { Authorization: token } });
-    console.log(res.data);
+    try {
+      const token = window.localStorage.getItem("token");
+      const res = await axios.post(`${URL}/api/product`, { url: productURL }, { headers: { Authorization: token } });
+      setErrorMessage("");
+      setSuccessMessage("Product added succesfully");
+      setInterval(() => {
+        setSuccessMessage("");
+      }, 3000);
+    } catch (error) {
+      setSuccessMessage("");
+      setErrorMessage("Wrong URL format");
+      setInterval(() => {
+        setErrorMessage("");
+      }, 3000);
+    }
   };
   return (
     <div>
       <h2>Add Product</h2>
+      <span style={{ color: "red" }}>{errorMessage}</span>
+      <span style={{ color: "green" }}>{successMessage}</span>
       <Form onSubmit={addProduct}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Product's URL (Amazon)</Form.Label>
-          <Form.Control type="text" placeholder="" onChange={(e) => setProductURL(e.target.value)} />
+          <Form.Control
+            type="text"
+            placeholder="https://www.amazon.com/Logitech-MK270-Wireless-Keyboard-Mouse/dp/B079JLY5M5/ref=lp_16225007011_1_8"
+            onChange={(e) => setProductURL(e.target.value)}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
