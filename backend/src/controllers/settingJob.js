bd0451b3237d84132430980bc179fe158598fadb
Diego <dcorreanavarrete@gmail.com>
2022-07-05 17:56:38 -0500

    add products url to the email text

--- backend/src/controllers/settingJob.js
+++ backend/src/controllers/settingJob.js
@@ -2,6 +2,8 @@ const schedule = require("node-schedule");
 const { Product, UserProducts } = require("../../models");
 const transport = require("../utils/transportMail");
 const getPercentage = require("../utils/getPercentage");
+const { fetchNewPrice, updateProductPrice } = require("./product");
+const { generateUrlFromId } = require("../utils/url");
 require("dotenv/config");
 
 // every two hours gets the total products and creates
@@ -9,7 +11,7 @@ require("dotenv/config");
 const setJob = async () => {
   const twoHoursExp = "0 */2 * * *";
   schedule.scheduleJob(twoHoursExp, async () => {
-    const time = 1000 * 60 * 60 * 2; // two hours in milliseconds
+    const time = 1000 * 60; // two hours in milliseconds
     const amountProducts = await Product.count();
     const milliseconds = Math.round(time / amountProducts);
     const products = await Product.findAll();
@@ -26,13 +28,17 @@ const setJob = async () => {
 
 const sendEmailToUsers = async ({ productID, title, price, prevPrice }) => {
   const users = await UserProducts.findAll({ where: { productID }, attributes: ["email"] });
+  const url = generateUrlFromId(productID);
+  console.log(url);
   users.map((user) => {
     const percentage = getPercentage(prevPrice, price);
     const options = {
       from: process.env.EMAIL_ADDRESS,
       to: user.dataValues.email,
       subject: `The price of this product has changed ${title}`,
-      text: `The new price is ${price} which is ${Math.abs(percentage)}%  ${percentage > 0 ? "more expensive" : "cheaper"} `,
+      text: `The new price is ${price} which is ${Math.abs(percentage)}%  ${
+        percentage > 0 ? "more expensive" : "cheaper"
+      } \n URL: ${url} `,
     };
     transport.sendMail(options, (err, info) => {
       if (err) {
