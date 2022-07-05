4f6a176f31dbee29484975d36629ebe4ea432578
Diego <dcorreanavarrete@gmail.com>
2022-07-05 18:32:04 -0500

    fixs

--- backend/src/utils/getPercentage.js
+++ backend/src/utils/getPercentage.js
@@ -1,6 +1,9 @@
 const getPercentage = (prevVal, newVal) => {
-  let percentage = -Math.round(100 - (newVal * 100) / prevVal);
-  return percentage;
+  if (newVal < prevVal) {
+    return -parseFloat((100 - (newVal * 100) / prevVal).toFixed(1));
+  } else {
+    return parseFloat(((newVal * 100) / prevVal).toFixed(1));
+  }
 };
 
 module.exports = getPercentage;
