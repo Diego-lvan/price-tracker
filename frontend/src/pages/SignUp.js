2250f46c69f13388bbb2e5ebf0eba0a12682596d
Diego <dcorreanavarrete@gmail.com>
2022-12-21 21:00:24 -0600

    redirect to add product page when creating a user

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -2,12 +2,20 @@ import React from "react";
 import AuthForm from "../components/Auth/AuthForm";
 import axios from "axios";
 import { URL } from "../config";
-
+import { useNavigate } from "react-router-dom";
 const SignUp = () => {
+  const navigate = useNavigate();
+
   const signUp = async (e, email, pwd, name) => {
     e.preventDefault();
     try {
       const res = await axios.post(`${URL}/api/auth/register`, { email, pwd, name });
+      const token = res?.data?.accessToken;
+      if (token) {
+        window.localStorage.setItem("token", token);
+        navigate("/addProduct");
+      }
+      console.log(res);
     } catch (error) {
       console.log(error);
     }
