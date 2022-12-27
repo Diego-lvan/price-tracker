b03c8cf37e5efc3c0921f127ce5627b16a5aa770
Diego <dcorreanavarrete@gmail.com>
2022-12-26 21:57:26 -0600

    adding .evn to the database config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,24 +1,10 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: "app_database",
+    username: process.env.DB_USER || "root",
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.DB_NAME,
     host: "db",
-    dialect: "mysql",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "price_tracker_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "price_tracker_production",
-    host: "127.0.0.1",
     dialect: "mysql",
   },
 };
