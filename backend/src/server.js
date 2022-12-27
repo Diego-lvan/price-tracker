35d2bd8d768f5a1c224e76d3e4918fc010e5413a
Diego <dcorreanavarrete@gmail.com>
2022-12-26 21:56:24 -0600

    adding 3000 port as default

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const { sequelize } = require("./models");
 require("dotenv/config");
 
-app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT || 3000, async () => {
   await sequelize.sync();
   console.log(`Listening on http://localhost:${process.env.PORT}`);
 });
