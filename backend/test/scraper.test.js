c0572fa46159abf8c04a809456a2d2d270d2e742
diego <dcorreanavarrete@gmail.com>
2023-05-06 18:40:50 -0600

    updating the url to test

--- backend/test/scraper.test.js
+++ backend/test/scraper.test.js
@@ -1,11 +1,13 @@
 const scraper = require("../src/controllers/scraper");
+jest.useFakeTimers();
 jest.setTimeout(25000);
 
 describe("getProductData", () => {
   describe("when sending a valid URL", () => {
     it("should return the correct data type", async () => {
-      const url = "https://www.amazon.com/dp/B088S3V3R4";
-      const product = await scraper(url, "B088S3V3R4");
+      const url = "https://www.amazon.com/gp/product/B08SVS99Q5/";
+      const product = await scraper(url, "B08SVS99Q5");
+      console.log(product);
       expect(typeof product.price).toBe("number");
       expect(typeof product.title).toBe("string");
       expect(typeof product.productID).toBe("string");
