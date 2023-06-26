10f39ff70cd79241025aaba8c1fe8848fe52c00b
diego <dcorreanavarrete@gmail.com>
2023-06-26 14:31:57 -0600

    adding idDomain to product as primary key

--- backend/src/models/product.js
+++ backend/src/models/product.js
@@ -7,15 +7,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ UserProducts, HistoryPrices }) {
+    static associate({ UserProducts, HistoryPrices, Domain }) {
       // define association here
       this.hasMany(UserProducts, { foreignKey: "productID" });
       this.hasMany(HistoryPrices, { foreignKey: "productID" });
+      this.belongsTo(Domain, { foreignKey: "idDomain" });
     }
   }
   Products.init(
     {
       productID: { type: DataTypes.STRING, primaryKey: true },
+      idDomain: { type: DataTypes.INTEGER, primaryKey: true },
       title: { type: DataTypes.STRING, allowNull: false },
     },
     {
